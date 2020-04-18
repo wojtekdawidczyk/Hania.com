52a91fab6677633c9837241616745bfedad4d0ae
Wojciech <wdczyk@gmail.com>
2020-04-18 12:11:40 +0100

    add 4 colours and colours animations

--- colors.js
+++ colors.js
@@ -1,4 +1,15 @@
+
+// var colorList = ["black", "blue", "brown" ]
+
 $("button").click(function(){
-  var audioColor = new Audio("sounds/" + this.name + "Wojtas.m4a");
+  var className = this.name;
+  var audioColor = new Audio("sounds/" + className + "Wojtas.m4a");
   audioColor.play();
+  $("." + className).addClass( className + "-active");
+  setTimeout(function(){ $("." + className).removeClass( className + "-active") }, 1000);
 });
+
+// $(".red").click(function() {
+//   $(".red").addClass("red-active");
+//   setTimeout(function(){ $(".red").removeClass("red-active") }, 1000);
+// });
